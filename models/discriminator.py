880d68c756bf2f60cc3ac26e5d8b525c94c7965d
Diogo Medeiros <diogo_medeiros@sapo.pt>
2022-12-14 17:39:08 +0000

    - removed unnecessary variable in create_discriminator()

--- models/discriminator.py
+++ models/discriminator.py
@@ -6,32 +6,30 @@ from tensorflow.keras.optimizers import Adam
 
 # define the standalone discriminator model
 def create_discriminator(image_size: int, channels: int, n_classes: int, lr: float, beta_1: float) -> Model:
-    # define input shape
-    in_shape = (image_size, image_size, channels)
     # label input
     in_label = Input(shape=(1,))
     # embedding for categorical input
     li = Embedding(n_classes, 50)(in_label)
     # scale up to image dimensions with linear activation
-    n_nodes = in_shape[0] * in_shape[1]
+    n_nodes = image_size * image_size
     li = Dense(n_nodes)(li)
     # reshape to additional channel
-    li = Reshape((in_shape[0], in_shape[1], 1))(li)
+    li = Reshape((image_size, image_size, 1))(li)
     # image input
-    in_image = Input(shape=in_shape)
+    in_image = Input(shape=(image_size, image_size, channels))
     # concat label as a channel
     merge = Concatenate()([in_image, li])
-    # downsample
+    # downsample to 16x16
     fe = Conv2D(128, (3, 3), strides=(2, 2), padding='same')(merge)
     fe = LeakyReLU(alpha=0.2)(fe)
-    # downsample
+    # downsample to 8x8
     fe = Conv2D(128, (3, 3), strides=(2, 2), padding='same')(fe)
     fe = LeakyReLU(alpha=0.2)(fe)
     # flatten feature maps
     fe = Flatten()(fe)
-    # dropout
+    # dropout layer
     fe = Dropout(0.4)(fe)
-    # output
+    # output layer nodes
     out_layer = Dense(1, activation='sigmoid')(fe)
     # define model
     model = Model([in_image, in_label], out_layer)
