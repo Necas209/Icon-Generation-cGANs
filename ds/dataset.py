3327216fb8031c8c8a44c69624e0c2437969f2c9
Diogo Medeiros <diogo_medeiros@sapo.pt>
2022-12-21 19:37:48 +0000

    check for python version

--- ds/dataset.py
+++ ds/dataset.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import os
+import sys
 from dataclasses import dataclass
-from typing import Any
 
 import numpy as np
 
@@ -57,7 +57,11 @@ class Icons50Dataset:
 def create_dataset(path: str | bytes | os.PathLike) -> Icons50Dataset:
     """ Create a dataset from a path """
     # Load the icons-50 dataset
-    icons: dict | Any = np.load(path, allow_pickle=True).item()
+    # check if python version is 3.8 or lower
+    if sys.version_info < (3, 9):
+        icons = np.load(path).item()
+    else:
+        icons = np.load(path, allow_pickle=True)
     # Convert the lists to numpy arrays
     icons = {k: np.array(v) for k, v in icons.items()}
     # Create the dataset
